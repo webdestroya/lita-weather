34f47dd85aa1460be38c437097e6611e955e162c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2015-04-28 02:20:13 -0700

    Use Lita 4 config and test API.

--- lib/lita/handlers/weather.rb
+++ lib/lita/handlers/weather.rb
@@ -3,10 +3,7 @@ require "lita"
 module Lita
   module Handlers
     class Weather < Handler
-
-      def self.default_config(config)
-        config.api_key = nil
-      end
+      config :api_key, type: String, required: true
 
       route %r{^weather ([0-9]{5})(|-[0-9]{4})$}i, :weather_zip, command: true, help: {
         "weather <zipcode|airport|city,state>" => "Returns current weather conditions for the specified location."
@@ -19,25 +16,20 @@ module Lita
       route %r{^weather (.+\s*,\s*[a-z]{2})$}i, :weather_city, command: true
 
       def weather_zip(response)
-        return if Lita.config.handlers.weather.api_key.nil?
         response.reply get_conditions(response.matches[0][0])
       end
 
       def weather_airport(response)
-        return if Lita.config.handlers.weather.api_key.nil?
         response.reply get_conditions(response.matches[0][0])
       end
 
       def weather_pws(response)
-        return if Lita.config.handlers.weather.api_key.nil?
         response.reply get_conditions(response.matches[0][0])
       end
 
       # For a city search, we send the request to google, and get the lat/lng pairs
       # then send the coordinates to weather underground
       def weather_city(response)
-        return if Lita.config.handlers.weather.api_key.nil?
-
         resp = http.get('http://maps.googleapis.com/maps/api/geocode/json',
           address: response.matches[0][0],
           sensor: 'false')
@@ -60,12 +52,12 @@ module Lita
 
       def get_conditions(query)
         # http://api.wunderground.com/api/KEY/conditions/q/37.7749295,-122.4194155.json
-        resp = http.get("http://api.wunderground.com/api/#{Lita.config.handlers.weather.api_key}/conditions/q/#{query}.json")
+        resp = http.get("http://api.wunderground.com/api/#{config.api_key}/conditions/q/#{query}.json")
 
         raise 'InvalidResponse' unless resp.status == 200
-        
+
         obj = MultiJson.load(resp.body)
-        
+
         # check for an error response
         if obj['response']['error']
           return obj['response']['error']['description']
