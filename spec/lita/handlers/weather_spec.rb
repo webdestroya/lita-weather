e19ad163baaac0910ec106feb42faf368985cbc0
Kyle Hasenkamp <kyle.hasenkamp@buckle.com>
2015-06-01 16:05:50 -0500

    add radar and alerts

--- spec/lita/handlers/weather_spec.rb
+++ spec/lita/handlers/weather_spec.rb
@@ -22,13 +22,19 @@ describe Lita::Handlers::Weather, lita_handler: true do
 
   it { doesnt_route_command("weather ca").to(:weather_city) }
 
+  it { is_expected.to route_command("radar Dallas,TX").to(:radar_city) }
+  it { is_expected.to route_command("alerts Dallas,TX").to(:alert_city) }
+
+  it { is_expected.not_to route_command("radar").to(:radar_city) }
+  it { is_expected.not_to route_command("12345").to(:radar_city) }
+  it { is_expected.not_to route_command("alerts").to(:alert_city) }
+  it { is_expected.not_to route_command("12345").to(:alert_city) }
 
   it "checks invalid zipcode" do
     send_command "weather 11111"
     expect(replies.last).to_not be_nil
     expect(replies.last).to eq("No cities match your search query")
   end
-
 
   it "checks valid zipcode" do
     send_command "weather 90210"
@@ -48,5 +54,16 @@ describe Lita::Handlers::Weather, lita_handler: true do
     expect(replies.last).to include("Los Angeles International")
   end
 
+  it "checks valid city for radar" do
+    send_command "radar Dallas,TX"
+    expect(replies.last).to_not be_nil
+    expect(replies.last).to include("Dallas")
+  end
+
+  it "checks valid city for alerts" do
+    send_command "alerts Dallas,TX"
+    expect(replies.last).to_not be_nil
+    expect(replies.last).to include("Dallas")
+  end
 
 end
